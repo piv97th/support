b12f064afd82e1cd3dce97940f64d0c80b2d92c7
piv97th <44411902+piv97th@users.noreply.github.com>
2020-04-18 22:33:07 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="ru">
+	
+	
+	<head>
+
+		<meta charset="utf-8">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<link rel="stylesheet" href="bootstrap.css" >
+		<link rel='stylesheet' href='style.css'>
+		
+		<script src="jquery-3.5.0.js"></script>
+		<script src="bootstrap.js"></script>
+		
+	</head>
+	
+	
+	<body>
+		
+		<div class="jumbotron text-center bg-light text-dark">
+			<h1>Веб-приложение</h1>
+			<p>Сопровождения итоговой государственной аттестации</p> 
+		</div>
+		
+		<nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+			<a class="navbar-brand" href="#">Главная</a>
+
+			<ul class="navbar-nav">
+
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+						Студенты
+					</a>
+					<div class="dropdown-menu">
+						
+						<div class="dropdown-header">На сайте</div>
+						<a class="dropdown-item" href="#">Добавить на сайте</a>
+						<a class="dropdown-item" href="#">Редактировать</a>
+						<a class="dropdown-item" href="#">Удалить</a>
+						<div class="dropdown-header">Через файл</div>
+						<a class="dropdown-item" href="#">Скачать файл</a>
+						<a class="dropdown-item" href="#">Загрузить бланк</a>
+					</div>
+				</li>
+				
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+						Пользователи
+					</a>
+					<div class="dropdown-menu">
+						
+						<!--<div class="dropdown-header">На сайте</div>-->
+						<a class="dropdown-item" href="#">Добавить</a>
+						<a class="dropdown-item" href="#">Редактировать</a>
+						<a class="dropdown-item" href="#">Удалить</a>
+					</div>
+				</li>
+				
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+						Комиссии
+					</a>
+					<div class="dropdown-menu">
+						
+						<!--<div class="dropdown-header">На сайте</div>-->
+						<a class="dropdown-item" href="#">Добавить</a>
+						<a class="dropdown-item" href="#">Редактировать</a>
+						<a class="dropdown-item" href="#">Удалить</a>
+					</div>
+				</li>
+				
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+						Госэкзамен
+					</a>
+					<div class="dropdown-menu">
+						
+						<!--<div class="dropdown-header">На сайте</div>-->
+						<a class="dropdown-item" href="#">Добавить</a>
+						<a class="dropdown-item" href="#">Редактировать</a>
+						<a class="dropdown-item" href="#">Удалить</a>
+					</div>
+				</li>
+				
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+						ВКР
+					</a>
+					<div class="dropdown-menu">
+						
+						<!--<div class="dropdown-header">На сайте</div>-->
+						<a class="dropdown-item" href="#">Добавить</a>
+						<a class="dropdown-item" href="#">Редактировать</a>
+						<a class="dropdown-item" href="#">Удалить</a>
+					</div>
+				</li>
+				
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+						Структура вуза
+					</a>
+					<div class="dropdown-menu">
+						
+						<!--<div class="dropdown-header">На сайте</div>-->
+						<a class="dropdown-item" href="#">Добавить</a>
+						<a class="dropdown-item" href="#">Редактировать</a>
+						<a class="dropdown-item" href="#">Удалить</a>
+					</div>
+				</li>
+				
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+						Члены ГЭК
+					</a>
+					<div class="dropdown-menu">
+						
+						<!--<div class="dropdown-header">На сайте</div>-->
+						<a class="dropdown-item" href="#">Добавить</a>
+						<a class="dropdown-item" href="#">Редактировать</a>
+						<a class="dropdown-item" href="#">Удалить</a>
+					</div>
+				</li>
+				
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+						Рецензия
+					</a>
+					<div class="dropdown-menu">
+						
+						<!--<div class="dropdown-header">На сайте</div>-->
+						<a class="dropdown-item" href="#">Добавить</a>
+						<a class="dropdown-item" href="#">Редактировать</a>
+						<a class="dropdown-item" href="#">Удалить</a>
+					</div>
+				</li>
+				
+				<li class="nav-item">
+					<a class="nav-link" href="#">Инфо</a>
+				</li>
+			</ul>
+		</nav>
+		
+		<div class="container-fluid text-center">    
+			<div class="row content">
+				<div class="col-sm text-left"> 
+					<h1>Welcome</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<hr>
+					<h3>Test</h3>
+					<p>Lorem ipsum...</p>
+				</div>
+			</div>
+		</div>
+
+		<footer class="container-fluid text-center bg-primary footer-dark">
+			<p>2020</p>
+		</footer>
+		
+	</body>
+</html>
