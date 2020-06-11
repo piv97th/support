c8a719a3acc6731f364776b666c0ee9b5a776ffd
piv97th <44411902+piv97th@users.noreply.github.com>
2020-06-11 17:36:53 +0300

    intime prot_se after spisok member without formatted

--- handler_document.php
+++ handler_document.php
@@ -136,9 +136,6 @@
 		$res_group = $conn->query('SELECT cathedra.name, cathedra.last_name, LEFT(cathedra.first_name, 1) as `first_name`, LEFT(cathedra.patronymic, 1) as `patronymic`, cathedra.abbreviation, group_1.cipher_group FROM group_1 INNER JOIN cathedra ON group_1.id_cathedra_fk = cathedra.id WHERE group_1.id = (SELECT id_group_fk FROM student WHERE id = '.$student.')');
 		$arr_group = $res_group->fetch_assoc();
 
-		//$result_member_ssk = $conn->query('SELECT last_name, LEFT(first_name, 1) as `first_name`, LEFT(patronymic, 1) as `patronymic` FROM member_ssk WHERE id IN (SELECT id_member_ssk_fk FROM curation_event WHERE role = 1 AND id_commission_fk IN(SELECT id FROM commission WHERE id IN(SELECT id_commission_fk FROM timetable_meeting WHERE id IN(SELECT id_meeting_diploma_fk FROM group_1 WHERE id = '.$arr_group["id"].'))))');
-		//$arr_member_ssk = $result_member_ssk->fetch_assoc();
-
 		require 'vendor/autoload.php';
 		require 'ncl_lib/NCLNameCaseRu.php';
 
@@ -219,36 +216,6 @@
 		$table->addCell(5000, array('valign' => 'center'))->addText('Зав. кафедрой '.$arr_group["abbreviation"], array('size' => 12), array('align'=>'center'));
 		$table->addCell(5000, array('valign' => 'center'))->addText($arr_group["last_name"].'.'.$arr_group["first_name"].'.'.$arr_group["patronymic"], array('size' => 12), array('align'=>'center'));
 
-		/*$table = $section->addTable(array('align' => 'center', 'valign' => 'center'));
-		$table->addRow(200);
-		$table->addCell(5000, array('valign' => 'center'))->addText('Направление', array('size' => 16), array('align'=>'center'));
-		$table->addCell(5000, array('valign' => 'center'))->addText($arr_group['cipher_direction'], array('size' => 16), array('align'=>'center'));
-
-		$section->addText("ПРЕДСЕДАТЕЛЮ ЭКЗАМЕНАЦИОННОЙ КОМИССИИ", $arr_style_text, $arr_paragraph_center);
-
-		$nc = new NCLNameCaseRu();
-		$last_name_cm = $arr_member_ssk['last_name'];
-		$first_name_cm = $arr_member_ssk['first_name'];
-		$patronymic_cm = $arr_member_ssk['patronymic'];
-
-		$section->addText($nc->qSecondName($last_name_cm, NCL::$DATELN).' '.$first_name_cm.'.'.$patronymic_cm.'.', array('name' => 'Arial', 'size' => 16, 'bold' => true), $arr_paragraph_center);
-		
-		$table = $section->addTable(array('align' => 'center', 'valign' => 'center'));
-		$table->addRow(200);
-		$table->addCell(4000, array('valign' => 'center'))->addText('Студент', array('size' => 16), array('align'=>'center'));
-		$table->addCell(8000, array('valign' => 'center'))->addText($arr_student["last_name"].' '.$arr_student["first_name"].' '.$arr_student["patronymic"], array('size' => 16, 'bold' => true), array('align'=>'center'));
-
-
-		$section->addText("Выполнил выпускную квалификационную работу на тему:", $arr_style_text, $arr_paragraph_center);
-		$section->addText($arr_diploma["topic"], array('name' => 'Arial', 'size' => 16, 'bold' => true), $arr_paragraph_center);
-		$section->addText("к защите выпускной квалификационной работы в Государственной экзаменационнойкомиссии ДОПУЩЕН", $arr_style_text, $arr_paragraph_center);
-
-		$table = $section->addTable(array('align' => 'center', 'valign' => 'center'));
-		$table->addRow(200);
-		$table->addCell(5000, array('valign' => 'center'))->addText('Зав. кафедрой '.$arr_group["abbreviation"], array('size' => 16), array('align'=>'center'));
-		$table->addCell(5000, array('valign' => 'center'))->addText($arr_group["last_name"].'.'.$arr_group["first_name"].'.'.$arr_group["patronymic"], array('size' => 16), array('align'=>'center'));*/
-
-
 		$file = 'Заключение_'.$arr_student["number_record_book"].'_'.$arr_student["last_name"].'_'.$arr_student["first_name"].'.docx';
 		header("Content-Description: File Transfer");
 		header('Content-Disposition: attachment; filename="' . $file . '"');
@@ -259,6 +226,114 @@
 		$xmlWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
 		$xmlWriter->save("php://output");
 		
+	}
+
+	if($_POST['doc'] == 3)
+	{
+		$student = $_POST['arr_1_student'];
+
+
+		$res_student = $conn->query('SELECT * FROM student WHERE id = '.$student);
+		$arr_student = $res_student->fetch_assoc();
+
+		// $res_group = $conn->query('SELECT cipher_group FROM group_1 WHERE id = '.$arr_student["id_group_fk"]);
+		// $arr_group = $res_group->fetch_assoc();
+
+		$res_diploma = $conn->query('SELECT number_protocol, topic, anti_plagiarism, id_kind_work_fk, id_teacher_fk FROM diploma WHERE id = '.$arr_student["id_diploma_fk"]);
+		$arr_diploma = $res_diploma->fetch_assoc();
+
+		$res_se = $conn->query('SELECT number_protocol FROM se WHERE id = '.$arr_student["id_se_fk"]);
+		$arr_se = $res_se->fetch_assoc();
+
+		$res_supervisor = $conn->query('SELECT last_name, LEFT(first_name, 1) as `first_name`, LEFT(patronymic, 1) as `patronymic`, degree, rank FROM teacher WHERE id = '.$arr_diploma["id_teacher_fk"]);
+		$arr_supervisor = $res_supervisor->fetch_assoc();
+
+		$res_group = $conn->query('SELECT cathedra.name, cathedra.last_name, LEFT(cathedra.first_name, 1) as `first_name`, LEFT(cathedra.patronymic, 1) as `patronymic`, cathedra.abbreviation, direction.cipher_direction, direction.name, group_1.id FROM group_1 INNER JOIN cathedra ON group_1.id_cathedra_fk = cathedra.id INNER JOIN direction ON group_1.id_direction_fk = direction.id WHERE group_1.id = (SELECT id_group_fk FROM student WHERE id = '.$student.')');
+		$arr_group = $res_group->fetch_assoc();
+
+		$res_meeting = $conn->query('SELECT date, SUBSTRING(date, 3, 2) as `year2` FROM timetable_meeting WHERE id IN (SELECT id_meeting_se_fk FROM group_1 WHERE id = '.$arr_group["id"].')');
+		$arr_meeting = $res_meeting->fetch_assoc();
+
+		$result_chairman = $conn->query('SELECT last_name, first_name, patronymic FROM member_ssk WHERE id IN (SELECT id_member_ssk_fk FROM curation_event WHERE role = 1 AND id_commission_fk IN(SELECT id FROM commission WHERE id IN(SELECT id_commission_fk FROM timetable_meeting WHERE id IN(SELECT id_meeting_diploma_fk FROM group_1 WHERE id = '.$arr_group["id"].'))))');
+		$arr_chairman = $result_chairman->fetch_assoc();
+
+		$result_member_ssk = $conn->query('SELECT last_name, LEFT(first_name, 1) as `first_name`, LEFT(patronymic, 1) as `patronymic` FROM member_ssk WHERE id IN (SELECT id_member_ssk_fk FROM curation_event WHERE role = 2 AND id_commission_fk IN(SELECT id FROM commission WHERE id IN(SELECT id_commission_fk FROM timetable_meeting WHERE id IN(SELECT id_meeting_diploma_fk FROM group_1 WHERE id = '.$arr_group["id"].'))))');
+		//$arr_member_ssk = $result_member_ssk->fetch_assoc();
+
+		require 'vendor/autoload.php';
+		require 'ncl_lib/NCLNameCaseRu.php';
+
+		$phpWord = new \PhpOffice\PhpWord\PhpWord();
+
+		$section = $phpWord->createSection();
+
+		$arr_style_text = array('name' => 'Arial', 'size' => 12);
+		$arr_paragraph_center = array('align' => 'center', 'lineHeight' => 1.0);
+		$arr_paragraph_left = array('align' => 'left', 'lineHeight' => 1.0);
+
+		$section->addImage('img/gerb.png', array('width' => 100, 'height' => 100, 'align'=>'center'));
+
+		$section->addText("МИНОБРНАУКИ РОССИИ", $arr_style_text, $arr_paragraph_center);
+		$section->addText("Федеральное государственное бюджетное образовательное учреждение", $arr_style_text, $arr_paragraph_center);
+		$section->addText("«МИРЭА - Российский технологический университет»", $arr_style_text, $arr_paragraph_center);
+		$section->addText("Институт комплексной безопасности и специального приборостроения", $arr_style_text, $arr_paragraph_center);
+		$section->addText("Кафедра $arr_group[name]", $arr_style_text, $arr_paragraph_center);
+
+		$table = $section->addTable(array('align' => 'center', 'valign' => 'center'));
+		$table->addRow(200);
+		$table->addCell(5000, array('valign' => 'center'))->addText($arr_group["cipher_direction"], array('size' => 12, 'bold' => true), array('align'=>'center'));
+		$table->addCell(5000, array('valign' => 'center'))->addText($arr_group["name"], array('size' => 12, 'bold' => true), array('align'=>'center'));
+
+		if(strlen($arr_se["number_protocol"]) == 1)
+		{
+			$protocol = '0'.$arr_se["number_protocol"];
+		}
+		else
+		{
+			$protocol = $arr_se["number_protocol"];
+		}
+		$section->addText('Протокол №'.$protocol.'/'.$arr_meeting["year2"], $arr_style_text, $arr_paragraph_center);
+
+		$section->addText("заседания Государственной экзаменационной комиссии", $arr_style_text, $arr_paragraph_center);
+
+		$section->addText(date('d.m.Y', $arr_meeting["date"]), $arr_style_text, $arr_paragraph_center);
+
+		$section->addText("по приему государственного аттестационного испытания —", $arr_style_text, $arr_paragraph_center);
+		$section->addText("государственного экзамена", $arr_style_text, $arr_paragraph_center);
+		$section->addText("Направление подготовки/специальность", $arr_style_text, $arr_paragraph_center);
+
+		$table = $section->addTable(array('align' => 'center', 'valign' => 'center'));
+		$table->addRow(200);
+		$table->addCell(5000, array('valign' => 'center'))->addText($arr_group["cipher_direction"], array('size' => 12), array('align'=>'center'));
+		$table->addCell(5000, array('valign' => 'center'))->addText($arr_group["name"], array('size' => 12), array('align'=>'center'));
+
+		$section->addText("ПРИСУТСТВОВАЛИ:", array('name' => 'Arial', 'size' => 12, 'bold' => true), $arr_paragraph_left);
+		$section->addText("Председатель Государственной экзаменационной комиссии:", $arr_style_text, $arr_paragraph_left);
+		$section->addText($arr_chairman["last_name"].' '.$arr_chairman["first_name"].' '.$arr_chairman["patronymic"], $arr_style_text, $arr_paragraph_left);
+		$section->addText("Члены Государственной экзаменационной комиссии ", $arr_style_text, $arr_paragraph_left);
+
+		//$listStyle = array('listType'=>PHPWord_Style_ListItem::TYPE_NUMBER);
+		while($arr_member_ssk = $result_member_ssk->fetch_assoc())
+		{
+			$section->addListItem($arr_member_ssk["last_name"].''.$arr_member_ssk["first_name"].''.$arr_member_ssk["patronymic"], 0);
+		}
+
+		$table = $section->addTable(array('align' => 'center', 'valign' => 'center'));
+		$table->addRow(200);
+		$table->addCell(5000, array('valign' => 'center'))->addText("Студент(ка)", array('size' => 12), array('align'=>'center'));
+		$table->addCell(5000, array('valign' => 'center'))->addText($arr_student["last_name"].' '.$arr_student["first_name"].' '.$arr_student["patronymic"], array('size' => 12), array('align'=>'center'));
+
+		$file = 'Протокол_ГЭ_'.$arr_student["number_record_book"].'_'.$arr_student["last_name"].'_'.$arr_student["first_name"].'.docx';
+		header("Content-Description: File Transfer");
+		header('Content-Disposition: attachment; filename="' . $file . '"');
+		header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+		header('Content-Transfer-Encoding: binary');
+		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+		header('Expires: 0');
+		$xmlWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+		$xmlWriter->save("php://output");
+
+
 	}
 
 	if($_POST['mode_other'] == 1)
