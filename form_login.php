99067572a6daeb23f4d61bc17499208a5be70826
piv97th <44411902+piv97th@users.noreply.github.com>
2020-06-12 22:23:58 +0300

    after cng log

--- form_login.php
+++ form_login.php
@@ -23,7 +23,6 @@
 		        	async: false,
 		        	success: function(response)
 		        	{
-		        		alert(response);
 		        		var result = JSON.parse(response);
 		        		outToast(result);
 		        		if(result['user'] == 1)
